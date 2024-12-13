47ec1ae111efc6403cf0c8477e217e264f3c0b0a
Nicopee <gidudunicholas11@gmail.com>
2024-12-13 23:34:09 +0300

    fix: added red color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color blueBG = Color.fromARGB(255, 8, 2, 74);
 const Color whiteBG = Colors.white;
 const Color blackBG = Colors.black;
+const Color redBG = Colors.red;
